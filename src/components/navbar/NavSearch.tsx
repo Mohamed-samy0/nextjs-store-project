1af7c153cc9fee64621cd4e72cc8b0a95387391e
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-03-17 14:19:02 +0200

    feat: initial commit

---
+++ src/components/navbar/NavSearch.tsx
@@ -0,0 +1,7 @@
+import { Input } from "../ui/input";
+
+function NavSearch() {
+  return <Input type="text" placeholder="Search products..." className="max-w-xs dark:bg-muted" />;
+}
+
+export default NavSearch;
