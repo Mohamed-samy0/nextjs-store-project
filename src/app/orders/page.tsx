e460e303c566c69681aafb43cf42a5d3c532ddb1
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-03-27 20:26:19 +0200

    Fix empty page modules to resolve build error

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,9 @@
+function OrdersPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Orders Page</h1>
+    </div>
+  );
+}
+
+export default OrdersPage;
---
+++ src/app/reviews/page.tsx
@@ -0,0 +1,9 @@
+function ReviewsPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Reviews Page</h1>
+    </div>
+  );
+}
+
+export default ReviewsPage;
