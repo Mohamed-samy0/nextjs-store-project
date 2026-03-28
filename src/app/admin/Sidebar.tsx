909a7f97a4f1d580e09975451077cca9b0b397e3
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-03-28 11:35:20 +0200

    Admin Pages - Structure

---
+++ src/app/admin/Sidebar.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { adminLinks } from "@/utils/links";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+function Sidebar() {
+  const pathName = usePathname();
+  return (
+    <aside>
+      {adminLinks.map((link) => {
+        const isActivePage = pathName === link.href;
+        const variant = isActivePage ? "default" : "ghost";
+        return (
+          <Button
+            asChild
+            className="w-full mb-2 capitalize font-normal"
+            variant={variant}
+            key={link.href}
+          >
+            <Link key={link.href} href={link.href}>
+              {link.label}
+            </Link>
+          </Button>
+        );
+      })}
+    </aside>
+  );
+}
+export default Sidebar;
