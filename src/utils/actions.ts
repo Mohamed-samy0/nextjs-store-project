50a1d521f3c77e09091986cefedd3fe43ccb9bc1
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-03-29 17:38:41 +0200

    CreateProductAction - First Approach + Helper

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,7 +1,19 @@
 "use server";
 
 import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+
+const authenticateUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/");
+  return user;
+};
+
+const renderErrorMessage = (error: unknown): { message: string } => {
+  console.log(error);
+  return { message: error instanceof Error ? error.message : "there was an error" };
+};
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -40,5 +52,27 @@ export const createProductAction = async (
   prevState: { message: string } | null,
   formData: FormData,
 ): Promise<{ message: string }> => {
-  return { message: "product created" };
+  const user = await authenticateUser();
+  try {
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    const price = Number(formData.get("price") as string);
+    const image = formData.get("image") as File;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: "/images/product-1.jpg",
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+    return { message: "product created" };
+  } catch (error) {
+    return renderErrorMessage(error);
+  }
 };
