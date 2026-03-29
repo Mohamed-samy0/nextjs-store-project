1897de9622aa18327f00b6c2c11388fa92d41334
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-03-29 15:59:11 +0200

    FormContainer Component

--- src/app/admin/products/create/page.tsx
+++ src/app/admin/products/create/page.tsx
@@ -1,14 +1,12 @@
+import FormContainer from "@/components/form/FormContainer";
 import FormInput from "@/components/form/FormInput";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { faker } from "@faker-js/faker";
-
-const createProductAction = async (formData: FormData) => {
-  "use server";
-  const name = faker.commerce.productName();
-  console.log(name);
-};
+import { createProductAction } from "@/utils/actions";
+import PriceInput from "@/components/form/PriceInput";
+import ImageInput from "@/components/form/ImageInput";
+import TextAreaInput from "@/components/form/TextAreaInput";
+import CheckboxInput from "@/components/form/CheckboxInput";
+import { SubmitButton } from "@/components/form/Button";
 
 function CreateProductPage() {
   const name = faker.commerce.productName();
@@ -18,12 +16,28 @@ function CreateProductPage() {
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">Create Product</h1>
       <div className="border rounded-md p-8">
-        <form action={createProductAction}>
-          <FormInput type="text" name="name" label="product name" defaultValue={name} />
-          <Button size="lg" type="submit">
-            Create Product
-          </Button>
-        </form>
+        <FormContainer action={createProductAction}>
+          <div className="grid md:grid-cols-2 gap-4 my-4">
+            <FormInput type="text" name="name" label="product name" defaultValue={name} />
+            <FormInput
+              type="text"
+              name="company"
+              label="company"
+              defaultValue={company}
+            />
+            <PriceInput />
+            <ImageInput />
+          </div>
+          <TextAreaInput
+            name="description"
+            labelText="description"
+            defaultValue={description}
+          />
+          <div className="mt-6">
+            <CheckboxInput name="featured" label="featured" />
+          </div>
+          <SubmitButton text="Create product" className="mt-8" />
+        </FormContainer>
       </div>
     </section>
   );
---
+++ src/components/form/Button.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { SignInButton } from "@clerk/nextjs";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { LuTrash2, LuSquarePen } from "react-icons/lu";
+import { Loader2 } from "lucide-react";
+
+type btnSize = "default" | "lg" | "sm";
+
+type SubmitButtonProps = {
+  className?: string;
+  text?: string;
+  size?: btnSize;
+};
+
+export function SubmitButton({
+  className = "",
+  text = "submit",
+  size = "lg",
+}: SubmitButtonProps) {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      size={size}
+      className={cn("capitalize", className)}
+      disabled={pending}
+    >
+      {pending ? (
+        <>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+}
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode="modal">
+      <Button
+        type="button"
+        size="icon"
+        variant="outline"
+        className="p-2 cursor-pointer"
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
+  );
+};
+
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      size="icon"
+      variant="outline"
+      className="p-2 cursor-pointer"
+    >
+      {pending ? (
+        <Loader2 className="animate-spin" />
+      ) : isFavorite ? (
+        <FaHeart className="text-red-500" />
+      ) : (
+        <FaRegHeart />
+      )}
+    </Button>
+  );
+};
