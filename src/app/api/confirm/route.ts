e852c912d0140d6be43569b53f4e438fbbc33177
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-04-10 13:15:41 +0200

    Confirm Route

--- src/app/api/confirm/route.ts
+++ src/app/api/confirm/route.ts
@@ -1,1 +1,37 @@
-export {};
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+import { redirect } from "next/navigation";
+
+import { NextResponse, type NextRequest } from "next/server";
+import db from "@/utils/db";
+
+export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const session_id = searchParams.get("session_id") as string;
+
+  try {
+    const session = await stripe.checkout.sessions.retrieve(session_id);
+    const orderId = session.metadata?.orderId;
+    const cartId = session.metadata?.cartId;
+
+    if (session.status === "complete") {
+      await db.order.update({
+        where: {
+          id: orderId,
+        },
+        data: {
+          isPaid: true,
+        },
+      });
+    }
+    await db.cart.delete({
+      where: {
+        id: cartId,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+  redirect("/orders");
+};
