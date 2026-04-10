1665f4c4dae713cdbb1bd75cafbffdb5224a097f
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-04-10 11:29:08 +0200

    Refactor Order Model and createOrderAction

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -549,8 +549,19 @@ export const createOrderAction = async (
   formData: FormData,
 ) => {
   const user = await authenticateUser();
+  let orderId: null | string = null;
+  let cartId: null | string = null;
   try {
     const cart = await fetchOrCreateCart({ userId: user.id, errorOnFailure: true });
+    cartId = cart.id;
+
+    await db.order.deleteMany({
+      where: {
+        clerkId: user.id,
+        isPaid: false,
+      },
+    });
+
     const order = await db.order.create({
       data: {
         clerkId: user.id,
@@ -561,15 +572,11 @@ export const createOrderAction = async (
         email: user.emailAddresses[0].emailAddress,
       },
     });
-    await db.cart.delete({
-      where: {
-        id: cart.id,
-      },
-    });
+    orderId = order.id;
   } catch (error) {
     return renderErrorMessage(error);
   }
-  redirect("/orders");
+  redirect(`/checkout?orderId=${orderId}&cartId=${cartId}`);
 };
 
 export const fetchUserOrders = async () => {
